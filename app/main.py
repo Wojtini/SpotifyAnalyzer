2d30d3443e4bd1dbbb529fef706b5f4b33f4e0da
aleks <alekskarbovskiy@gmail.com>
2024-05-27 17:29:12 +0200

    Added Spotify OAuth

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
-from app.routers.hello_world import router as hello_world_router
+from app.routers.oauth import router as oauth_router
 
 app = FastAPI()
 
-app.include_router(hello_world_router)
+app.include_router(oauth_router)
--- app/routers/hello_world.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-
-@router.get("/")
-def hello_world() -> dict[str, str]:
-    return {"message": "Hello World"}
