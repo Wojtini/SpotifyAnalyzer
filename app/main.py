ab34a0e3678029d0ea4ccee364576b3610a7d72a
Aleksey9777 <alekskarbovskiy@gmail.com>
2024-06-10 19:41:28 +0200

    ok/feat/oauth2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 
+from app.routers.oauth import router as oauth_router
 from app.routers.recommendations import api
 
 app = FastAPI()
 app.include_router(api)
+app.include_router(oauth_router)
+
+app = FastAPI()
